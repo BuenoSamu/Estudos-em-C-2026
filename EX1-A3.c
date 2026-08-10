52f3abdda307a7545753f685359b5667bc17e25b
BuenoSamu <samuelbueno467@gmail.com>
2026-08-10 12:00:19 -0300

    first commit

---
+++ EX1-A3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int primeiro_numero;
+    int segundo_numero;
+    int terceiro_numero;
+    int quarto_numero;
+    int quinto_numero;
+    int resultado;
+
+    printf("Entre com os 5 numeros: \n");
+
+    scanf("%d %d %d %d %d",
+          &primeiro_numero,
+          &segundo_numero,
+          &terceiro_numero,
+          &quarto_numero,
+          &quinto_numero);
+
+    resultado = (primeiro_numero + segundo_numero + terceiro_numero + quarto_numero + quinto_numero) / 5;
+
+    printf("A media e: %d\n", resultado);
+
+    return 0;
+}
